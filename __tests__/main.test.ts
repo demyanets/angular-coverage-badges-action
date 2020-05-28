aaa002e6e0e30d381f0f0708676524666f333633
Taras Demyanets <mail4github@gmail.com>
2020-05-28 19:35:14 +0200

    More precise imports

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,19 +1,18 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import {env} from 'process'
+import {ExecSyncOptions, execSync} from 'child_process'
+import {join} from 'path'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_COVERAGE-SUMMARY-PATH'] =
-    './coverage/coverage-summary.json'
-  process.env['BADGES-DIRECTORY'] = './badges'
-  process.env['INPUT_PROTECTED-BRANCHES'] = '["master", "develop"]'
-  const options: cp.ExecSyncOptions = {
-    env: process.env
+  env['INPUT_COVERAGE-SUMMARY-PATH'] = './coverage/coverage-summary.json'
+  env['BADGES-DIRECTORY'] = './badges'
+  env['INPUT_PROTECTED-BRANCHES'] = '["master", "develop"]'
+  const options: ExecSyncOptions = {
+    env: env
   }
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = join(__dirname, '..', 'lib', 'main.js')
   const cmd = `node "${ip}"`
   console.log(cmd)
-  var output = cp.execSync(cmd, options)
+  var output = execSync(cmd, options)
   console.log(output.toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import * as core from '@actions/core'
+import {debug, setFailed} from '@actions/core'
 import {Inputs} from './inputs'
 
 async function run(): Promise<void> {
   try {
     const inputs = new Inputs()
-    core.debug(`coverage-summary-path: ${inputs.coverageSummaryPath}`)
-    core.debug(`badges-directory: ${inputs.badgesDirectory}`)
+    debug(`coverage-summary-path: ${inputs.coverageSummaryPath}`)
+    debug(`badges-directory: ${inputs.badgesDirectory}`)
     for (const branch of inputs.protectedBranches) {
-      core.debug(`Protected branch: ${branch}`)
+      debug(`Protected branch: ${branch}`)
     }
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
