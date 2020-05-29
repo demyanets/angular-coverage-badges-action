44ba985eda568551ddc09b84d1821a153cc5f996
Taras Demyanets <mail4github@gmail.com>
2020-05-29 18:46:19 +0200

    Additional coverage  asserts

--- src/get-badge-color.ts
+++ src/get-badge-color.ts
@@ -5,6 +5,8 @@ export function getBadgeColor(
   thresholdRed = 80,
   thresholdYellow = 90
 ): 'red' | 'yellow' | 'brightgreen' {
+  assert(coverage >= 0, 'Coverage shall be greater or equal 0')
+  assert(coverage <= 100, 'Coverage shall be lesser or equal 100')
   assert(thresholdRed > 0, 'Red threshold shall be greater than 0')
   assert(thresholdRed < 100, 'Red threshold shall be lesser than 100')
   assert(thresholdYellow > 0, 'Yellow threshold shall be greater than 0')
