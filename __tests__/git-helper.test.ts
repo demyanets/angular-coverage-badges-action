857ddd527b24c533421f35430d29cc3a8b4da4f4
Taras Demyanets <mail4github@gmail.com>
2020-05-31 23:10:37 +0200

    stdout interpreting tests

--- __tests__/git-helper.test.ts
+++ __tests__/git-helper.test.ts
@@ -9,6 +9,8 @@ describe('Git helper tests', () => {
     expect(result.length).toBe(2)
     expect(result[0].length).toBeGreaterThan(0)
     expect(result[1]).toEqual('')
+    //const branch = result[0]
+    //console.log(`Current branch: ${branch.trim()}`)
   })
 
   test('should return current branch', async () => {
@@ -18,5 +20,7 @@ describe('Git helper tests', () => {
     console.log(JSON.stringify(result))
     expect(result[0]).toEqual('')
     expect(result[1]).toEqual('')
+    //const matches = (result[0].match(/\.svg/g) || []).length
+    //console.log(`SVG matches: ${matches}`)
   })
 })
