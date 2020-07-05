cd6e31001c0a09e5f7b49515d002c7071758d26d
Taras Demyanets <mail4github@gmail.com>
2020-07-05 11:05:00 +0200

    Robuster persist test

--- __tests__/persist.test.ts
+++ __tests__/persist.test.ts
@@ -3,12 +3,22 @@ import {persist} from '../src/persist'
 import {normalize, join} from 'path'
 
 describe('Persist tests', () => {
+  let expectedFilePath
+  let path
+  let fileName
+
+  beforeEach(() => {
+    path = normalize('__tests__/temp')
+    fileName = 'coverage-badge.svg'
+    expectedFilePath = normalize(join(path, fileName))
+    if (existsSync(expectedFilePath)) {
+      unlinkSync(expectedFilePath)
+    }
+  })
+
   test('should persist', async () => {
-    const path = normalize('__tests__/temp')
-    const fileName = 'coverage-badge.svg'
     const content =
       '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="106" height="20"></svg>'
-    const expectedFilePath = normalize('__tests__/temp/coverage-badge.svg')
     expect(existsSync(expectedFilePath)).toBeFalsy()
     const fullName = await persist(content, path, fileName, true)
     expect(fullName).toEqual(join(path, fileName))
