c1c4c630c4309c6733fa7d2f2a73e1274d860bfe
Taras Demyanets <20560928+demyanets@users.noreply.github.com>
2026-04-05 19:03:20 +0200

    Updated to new coverage-summary.json

--- __tests__/git-utilities.test.ts
+++ __tests__/git-utilities.test.ts
@@ -1,15 +1,21 @@
+import {jest} from '@jest/globals'
 import {join} from 'path'
-import {
-  getDiffs,
-  getGitVersion,
-  getBranch,
-  isBranchPushable
-} from '../src/git-utilities.js'
 import {ExecOptionsStub} from '../src/exec-options-stub.js'
+
+const mockExec = jest.fn<typeof import('@actions/exec').exec>()
+jest.unstable_mockModule('@actions/exec', () => ({exec: mockExec}))
+
+const {getDiffs, getGitVersion, getBranch, isBranchPushable} = await import(
+  '../src/git-utilities.js'
+)
 
 describe('Git helper tests', () => {
   test('get git version', async () => {
     const stub = new ExecOptionsStub()
+    mockExec.mockImplementationOnce(async (_tool, _args, options) => {
+      options?.listeners?.stdout?.(Buffer.from('git version 2.50.1\n'))
+      return 0
+    })
     const result = await getGitVersion(stub.options)
     expect(result).toEqual(0)
     expect(stub.stdout.startsWith('git version')).toBeTruthy()
@@ -25,6 +31,7 @@ describe('Git helper tests', () => {
   test('should not find any differences', async () => {
     const stub = new ExecOptionsStub()
     const dir = join('__tests__', 'assets')
+    mockExec.mockResolvedValueOnce(0)
     const result = await getDiffs(dir, stub.options)
     expect(result).toEqual(0)
     expect(stub.stdout).toEqual('')
